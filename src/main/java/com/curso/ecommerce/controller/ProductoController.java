6431a685c51fc3d515930d0a0b598af1a4455750
Marglodis Tovar <marglodistovar@gmail.com>
2023-10-08 18:32:32 -0300

    Funcionalidad para mostrar productos/imagenes desde la vista home del administrador

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -24,6 +24,7 @@ public class ProductoController {
 	@Autowired
 	private ProductoService productoService;
 
+	@Autowired
 	private UploadFileService upload;
 	
 	@GetMapping("")
@@ -46,15 +47,7 @@ public class ProductoController {
 		if(producto.getId()==null){ // Es cuando se crea un producto
 			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
-		}else{
-			if(file.isEmpty()){ //Editamos un producto pero no cambiamos la imagen
-				Producto p = new Producto();
-				p = productoService.get(producto.getId()).get();
-				producto.setImagen(p.getImagen());
-			}else{
-				String nombreImagen = upload.saveImage(file);
-				producto.setImagen(nombreImagen);
-			}
+
 		}
 
 		productoService.save(producto);
@@ -72,13 +65,37 @@ public class ProductoController {
 	}
 
 	@PostMapping("/update")
-	public String update(Producto producto){
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		Producto p = new Producto();
+		p = productoService.get(producto.getId()).get();
+		if(file.isEmpty()){ //Editamos un producto pero no cambiamos la imagen
+
+			producto.setImagen(p.getImagen());
+		}else{ //Edita la imagen tambien
+
+
+			//Eliminar cuando no sea la imagen por defecto
+			if(!p.getImagen().equals("default.jpg")){
+				upload.deleteImage(p.getImagen());
+			}
+			String nombreImagen = upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
 
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id){
+		Producto p = new Producto();
+		p=productoService.get(id).get();
+
+		//Eliminar cuando no sea la imagen por defecto
+		if(!p.getImagen().equals("default.jpg")){
+			upload.deleteImage(p.getImagen());
+		}
+
 		productoService.delete(id);
 		return ("redirect:/productos");
 	}
